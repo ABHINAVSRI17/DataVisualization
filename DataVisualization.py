8f7e0045812a2ef9c5e517c5efb3af00fd373128
ABHINAVSRI17 <abhinavsri.srivastava@gmail.com>
2020-05-28 20:28:38 +0530

    adding DataVisualization.py

--- DataVisualization.py
+++ DataVisualization.py
@@ -1,14 +1,14 @@
 print("abhinav")
-#tup does not allow changes in the list unlike list
-#[] for list
-#() for tupels or tup
-#{} for sets
-#tup and list are similar but tup values cannot be changed
-#in set it removes repetitive values and it will not follow the sequence
-#for tracking the address of the values use id(val
+# tup does not allow changes in the list unlike list
+# [] for list
+# () for tupels or tup
+# {} for sets
+# tup and list are similar but tup values cannot be changed
+# in set it removes repetitive values and it will not follow the sequence
+# for tracking the address of the values use id(val)
 num = 5
 print(id(num))
-#assigning the same values to the different variable will make both the variables pointing at the same address
+# assigning the same values to the different variable will make both the variables pointing at the same address
 a = 5
 b = a
 print(a)
@@ -19,17 +19,40 @@ print(id(b))
 # we cannot make constant in python but can only show intention of making constant
 PI = 3.14
 print(PI)
-#we can know the data type of variable by using type() command
+# we can know the data type of variable by using type() command
 print(type(PI))
 # numeric data types : int float complex bool(boolean)
-a=4
+a = 4
 print(type(a))
-b=3.5
+b = 3.5
 print(type(b))
-c=(6+5j)
+c = (6+5j)
 print(type(c))
 # we can change the type of data into another type of data
 print(int(b))
+bool = b > a
+print(bool)
+bool = 11 > a
+print(bool)
+
+z = "abhinav"
+print(type(z))
+# range
+print(range(17))
+# listing of range
+print(list(range(17)))
+# printing the even no. between 2-17 (2,17,2) [2=starting no.],[10=ending no.],[2= difference between no.]
+print(list(range(2, 17,2)))
+print(type(range(17)))
+# dictionary
+d = {'student1': 'python', 'student2': 'c++', 'student3': 'java'}
+print(d)
+print(d.keys())
+print(d.values())
+print(d['student1'])
+print(d.get('student2'))
+# swapping the values
+
 
 
 
